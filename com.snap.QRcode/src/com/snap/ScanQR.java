1e0ee33186866d6178bb6c8d157fd52c52f4f85c
Lukasz <lukasz@ubuntu.(none)>
2012-09-26 21:42:26 +0200

    scan_qr layout almost completed

---
+++ com.snap.QRcode/src/com/snap/ScanQR.java
@@ -0,0 +1,16 @@
+package com.snap;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ScanQR extends CaptureActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_qr);
+    }
+
+}

8040cc4d790a3af7c463ed41a71ba974fffe43cf
Lukasz <lukasz@ubuntu.(none)>
2012-10-07 18:17:11 +0200

    snap with map

--- com.snap.QRcode/src/com/snap/Map.java
+++ com.snap.QRcode/src/com/snap/Map.java
@@ -1,15 +1,32 @@
 package com.snap;
 
 
-import android.app.Activity;
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class Map extends Activity{
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+
+public class Map extends MapActivity implements LocationListener {
+	
+	MapView map = null;
+	MyLocationOverlay overlay;
+	LocationManager locationManager;
+	String bestProvider;
+	MapController mapController;
 	
 	ImageButton mapBtn;
 	ImageButton listBtn;
@@ -25,6 +42,23 @@ public class Map extends Activity{
         
         context = getApplicationContext();
         
+        map = (MapView) findViewById(R.id.map); 
+        mapController = map.getController();
+        
+        mapController.setZoom(16);
+        map.setBuiltInZoomControls(true);
+        map.setSatellite(false);
+        
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+		bestProvider = locationManager.getBestProvider(criteria, false);
+		
+		Location location = locationManager.getLastKnownLocation(bestProvider);
+		GeoPoint point = new GeoPoint((int)(location.getLatitude() * 1E6), (int)(location.getLongitude() * 1E6));
+		Log.d("GPS", ""+point);   
+		
+		mapController.animateTo(point);
+        
         mapBtn = (ImageButton)findViewById(R.id.map_btn);
         listBtn = (ImageButton)findViewById(R.id.list_btn);
         cardBtn = (ImageButton)findViewById(R.id.card_btn);
@@ -35,7 +69,30 @@ public class Map extends Activity{
         
         cardBtn.setOnClickListener(lCard);
         listBtn.setOnClickListener(lList);
+        
+        GetDataTask getDataTask = new GetDataTask(this);
+        getDataTask.execute("http://peaceful-hollows-9449.herokuapp.com/partners.json");
     }
+    
+    public void makeOverlay(ArrayList<PartnerPoint> partnerPoints) {
+    	
+		// dodajemy naszą warstwę do mapy
+		map.getOverlays().add(new PartnersOverlay(this, partnerPoints));
+	}
+    
+    @Override
+	protected void onPause() {
+		super.onPause();	
+		//overlay.disableMyLocation();
+		locationManager.removeUpdates(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		//overlay.enableMyLocation();
+		locationManager.requestLocationUpdates(bestProvider, 20000, 1, this);
+	}
 
 
     private View.OnClickListener lCard = new View.OnClickListener() {
@@ -55,4 +112,33 @@ public class Map extends Activity{
 			startActivity(i);
 		}
 	};
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
 }
